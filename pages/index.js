ce8f11a65c18f33f71afb64917a87339d9568441
ianbatoon <christianb.odonio@gmail.com>
2022-12-14 13:08:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Title from '../components/Title.jsx';
+import Nav from '../components/Nav.jsx';
+
+export default function index() {
+  return (
+    <div>
+      <Nav />
+      <Title />
+    </div>
+  );
+}
